718e2c42383de8bf7698aa67e0be767d9d9441b3
franciscamac <franpms89@gmail.com>
2025-11-01 09:00:29 -0300

    Arrange, Act, Assert and DisplayName

--- src/test/java/br/com/code/narf/automated_test_with_java_code_narf/math/SimpleMathTest.java
+++ src/test/java/br/com/code/narf/automated_test_with_java_code_narf/math/SimpleMathTest.java
@@ -2,18 +2,31 @@ package br.com.code.narf.automated_test_with_java_code_narf.math;
 
 import br.com.code.narf.automated_test_with_java_code_narf.SimpleMath;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@DisplayName("Test Math Operations in SimpleMath Class")
 public class SimpleMathTest {
+    //test[System Under Test]_[Condition]_[Expected Result]
     @Test
-    void testSum(){
+    @DisplayName("Sum of 6.2 and 2 should be 8.2")
+    void testSum_When_SixDotTwoIsAddedByTwo_ShouldReturnEightDotTwo(){
+        // AAA
+        // Arrange, Act, Assert
+        //Given / Arrange
         SimpleMath simpleMath = new SimpleMath();
+
         double fistNumber = 6.2D;
         double secondNumber = 2D;
+        double expected = 8.2D;
+
+        //When / Act
         Double actual = simpleMath.sum(fistNumber, secondNumber);
-        Double expected = 8.2D;
+
+
+        //Then  // Assert
         assertEquals(expected, actual, () -> fistNumber + " + " +secondNumber + " did not produce " + expected);
 
 //        assertNotEquals(9.2, actual);
@@ -21,6 +34,7 @@ public class SimpleMathTest {
     }
 
     @Test
+    @DisplayName("Subtraction of 2 from 6.2 should be 4.2")
     void testSubtraction(){
         SimpleMath simpleMath = new SimpleMath();
         double fistNumber = 6.2D;
@@ -31,6 +45,7 @@ public class SimpleMathTest {
     }
 
     @Test
+    @DisplayName("Multiplication of 6.2 by 2 should be 12.4")
     void testMultiplication(){
         SimpleMath simpleMath = new SimpleMath();
         double fistNumber = 6.2D;
@@ -40,6 +55,7 @@ public class SimpleMathTest {
         assertEquals(expected, actual, () -> fistNumber + " * " +secondNumber + " did not produce " + expected);
     }
     @Test
+    @DisplayName("Division of 6.2 by 2 should be 3.1")
     void testDivision(){
         SimpleMath simpleMath = new SimpleMath();
         double fistNumber = 6.2D;
@@ -48,7 +64,18 @@ public class SimpleMathTest {
         Double expected = 3.1D;
         assertEquals(expected, actual, () -> fistNumber + " / " +secondNumber + " did not produce " + expected);
     }
+    @Disabled("Todo: We need stoll work on it")
     @Test
+    @DisplayName("Division of a number by zero should throw Ar6ithmeticException")
+    void testDivision_When_firstNunberIsDividedByZero_ShouldThrowArithmeticException() {
+        SimpleMath simpleMath = new SimpleMath();
+        double fistNumber = 6.2D;
+        double secondNumber = 0D;
+    }
+
+
+    @Test
+    @DisplayName("Median of an array of numbers should return the correct median value")
     void testMedian(){
         SimpleMath simpleMath = new SimpleMath();
         Double[] numbers = {1D, 3D, 2D, 5D, 4D};
@@ -56,4 +83,5 @@ public class SimpleMathTest {
         Double expected = 3D;
         assertEquals(expected, actual, () -> "The median of " + java.util.Arrays.toString(numbers) + " did not produce " + expected);
     }
+
 }
