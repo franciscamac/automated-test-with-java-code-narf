91ea9966c57d2207f8b7e0b32a8868d2e44f3f0a
franciscamac <franpms89@gmail.com>
2025-11-03 11:45:00 -0300

    working with @TestMethodOrder

---
+++ src/test/java/br/com/code/narf/automated_test_with_java_code_narf/MethodOrderedRandonlyTest.java
@@ -0,0 +1,25 @@
+package br.com.code.narf.automated_test_with_java_code_narf;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.Random.class)
+public class MethodOrderedRandonlyTest {
+    @Test
+    void test1(){
+        System.out.println("Running test 1");
+    }
+    @Test
+    void test2() {
+        System.out.println("Running test 2");
+    }
+    @Test
+    void test3() {
+        System.out.println("Running test 3");
+    }
+    @Test
+    void test4() {
+        System.out.println("Running test 4");
+    }
+}
