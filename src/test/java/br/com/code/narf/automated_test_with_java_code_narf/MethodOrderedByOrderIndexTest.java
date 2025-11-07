692aa1f6526651592eafa821a3074e8ae2941548
franciscamac <franpms89@gmail.com>
2025-11-07 11:08:30 -0300

    test life cycle with @TestInstance for class

--- src/test/java/br/com/code/narf/automated_test_with_java_code_narf/MethodOrderedByOrderIndexTest.java
+++ src/test/java/br/com/code/narf/automated_test_with_java_code_narf/MethodOrderedByOrderIndexTest.java
@@ -1,30 +1,41 @@
 package br.com.code.narf.automated_test_with_java_code_narf;
 
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
-@Order(3)
+//@Order(3)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(org.junit.jupiter.api.MethodOrderer.OrderAnnotation.class)
-public class MethodOrderedByOrder {
+public class MethodOrderedByOrderIndexTest {
+
+    StringBuilder actualValue = new StringBuilder("");
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("The actual value is: " + actualValue.toString());
+    }
+
     @Test
     @Order(1)
     void testC() {
         System.out.println("Running test C");
+        actualValue.append("1");
     }
     @Test
     @Order(2)
     void testD() {
         System.out.println("Running test D");
+        actualValue.append("2");
     }
     @Test
     @Order(3)
     void testA(){
         System.out.println("Running test A");
+        actualValue.append("3");
     }
     @Test
     @Order(4)
     void testB() {
         System.out.println("Running test B");
+        actualValue.append("4");
     }
 }
